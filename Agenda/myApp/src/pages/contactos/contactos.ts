5c2877f45f8431d17353431c6ed64dd81959e9ae
Lizaveta <100317944@alumnos.uc3m.es>
2018-04-04 15:32:34 +0200

    Added form to 'Contactos'

--- Agenda/myApp/src/pages/contactos/contactos.ts
+++ Agenda/myApp/src/pages/contactos/contactos.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import {ContactService} from '../../services/contacts.service';
+import {Contact} from '../../models/contact.model';
 /**
  * Generated class for the ContactosPage page.
  *
@@ -15,11 +16,16 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class ContactosPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, private contactService: ContactService) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ContactosPage');
+  }
+
+  onAddContact(value:Contact) {
+    this.contactService.addContact(value);
+    this.navCtrl.pop();
   }
 
 }
